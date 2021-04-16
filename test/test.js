18bed7a480b9329a038dc65cb9ff65734376721e
Rick <dirklo@gmail.com>
2021-04-16 10:40:00 +0200

    commit tree e352e5a1b4efdb66258ef40053d6c650c3731976

---
+++ test/test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { expect } from 'chai';
+
+
+describe('MyFace', () => {
+  it('should have one button', () => {
+    expect(["1", "2", "3"]).to.include("1");
+  });
+});
